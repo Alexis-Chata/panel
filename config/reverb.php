191302f06905e71ec8aad485def2a19b7aa65c14
rudranthonny <ing.anthonny.joel@gmail.com>
2025-09-13 13:40:20 -0500

    correcion del reverveb

--- config/reverb.php
+++ config/reverb.php
@@ -83,7 +83,7 @@ return [
                     'scheme' => env('REVERB_SCHEME', 'https'),
                     'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
                 ],
-                'allowed_origins' => $origins,
+                'allowed_origins' => ['*'],
                 'ping_interval' => env('REVERB_APP_PING_INTERVAL', 60),
                 'activity_timeout' => env('REVERB_APP_ACTIVITY_TIMEOUT', 30),
                 'max_message_size' => env('REVERB_APP_MAX_MESSAGE_SIZE', 10_000),
