aa4e32ef5aa16db900521ef59e35622a58c01c41
rudranthonny <ing.anthonny.joel@gmail.com>
2025-09-13 13:47:16 -0500

    actualizar

---
+++ app/Events/PingTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class PingTest
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn()  { return ['public.ping']; }
+    public function broadcastAs()  { return 'pong'; }
+    public function broadcastWith(){ return ['time' => now()->toIso8601String()]; }
+}
