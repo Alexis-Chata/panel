fb6bad881fb4784787205a194ee3f0099433b1b4
rudranthonny <ing.anthonny.joel@gmail.com>
2025-09-13 14:01:17 -0500

    actualizar

--- app/Events/PingTest.php
+++ app/Events/PingTest.php
@@ -2,18 +2,27 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
-use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
 
 class PingTest implements ShouldBroadcastNow
 {
-    public function broadcastOn()  { return ['public.ping']; }
-    public function broadcastAs()  { return 'pong'; } // si escuchas con .pong en el cliente
-    public function broadcastWith(){ return ['time' => now()->toIso8601String()]; }
+    use InteractsWithSockets;
+
+    public function broadcastOn(): array
+    {
+        // canal público simple
+        return ['public.ping'];
+    }
+
+    public function broadcastAs(): string
+    {
+        // alias del evento
+        return 'pong';
+    }
+
+    public function broadcastWith(): array
+    {
+        return ['time' => now()->toIso8601String()];
+    }
 }
